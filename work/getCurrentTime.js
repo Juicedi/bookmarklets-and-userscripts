03d55320b4bafedbdcc9a6dd6abf150b02567366
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2019-09-20 22:56:29 +0300

    refactored code to allow multiple timers

--- work/getCurrentTime.js
+++ work/getCurrentTime.js
@@ -1,73 +1,76 @@
-(function() {
-  'use strict';
-  const userId = ''; // FIXME: Fill in correct user ID
-  let apiUrl = '';   // FIXME: Fill in correct API URL
-  let timeoutTimes = 0, timeElem = null, today = '', url = '';
-  const TIMEOUTLIMIT = 20;
-  let xhttp = new XMLHttpRequest();
+(function main() {
+  // const userId = ''; // FIXME: Fill in correct user ID
+  // let apiUrl = '';   // FIXME: Fill in correct API URL
+  const container = document.createElement('DIV');
+  let todayTimeElement = null;
 
   function formatDate(date) {
-    var d = new Date(date), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear();
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+    const d = new Date(date);
+    const year = d.getFullYear().toString();
+    let month = (d.getMonth() + 1).toString();
+    let day = d.getDate().toString();
+
+    if (month.length < 2) month = `0${month}`;
+    if (day.length < 2) day = `0${day}`;
+
     return [year, month, day].join('');
   }
 
-  function sendRequest() {
+  function combineTimes(data) {
+    let combinedTimes = 0;
+    let times = [];
+
+    times = JSON.parse(data).timeEntries;
+    combinedTimes = times.reduce((start, obj) => start + obj.hours + (obj.minutes / 60), 0);
+    return Math.floor(combinedTimes * 100) / 100;
+  }
+
+  function handleResponse(target, text) {
+    const element = target;
+
+    if (this.readyState === 4 && this.status === 200) {
+      element.innerHTML = text + combineTimes(this.response);
+    }
+  }
+
+  function getTodaysTime(text, element) {
+    const xhttp = new XMLHttpRequest();
+    let today = '';
+    let url = '';
+
     today = formatDate(Date.now());
-    url = apiUrl + '?page=1&pageSize=100&getTotals=true&projectId=&companyId=0'
-      + '&userId=' + userId + '&invoicedType=all&billableType=all'
-      + '&fromDate=' + today + '&toDate=' + today
-      + '&sortBy=date&sortOrder=desc'
+    url = `${apiUrl}?page=1&pageSize=100&getTotals=true&projectId=&companyId=0`
+      + `&userId=${userId}&invoicedType=all&billableType=all`
+      + `&fromDate=${today}&toDate=${today}&sortBy=date&sortOrder=desc`
       + '&onlyStarredProjects=false&includeArchivedProjects=false'
       + '&matchAllTags=true&projectStatus=all';
-    xhttp.open("GET", url, true);
+    xhttp.open('GET', url, true);
+    xhttp.onreadystatechange = handleResponse.bind(xhttp, element, 'Today: ');
     xhttp.send();
   }
 
-  function createTimeElem() {
-    timeElem = document.createElement('DIV');
-    timeElem.style.position = 'absolute';
-    timeElem.style.color = '#DC7391';
-    timeElem.style.backgroundColor = '#B4526E';
-    timeElem.style.borderRadius = '3px';
-    timeElem.style.zIndex = 1000;
-    timeElem.style.top = '80px';
-    timeElem.style.left = '50%';
-    timeElem.style.padding = '0px 5px';
-    timeElem.style.transform = 'translateX(-50%)';
-    timeElem.style.cursor = 'pointer';
-    timeElem.title = "Double click to reload the data";
-    timeElem.addEventListener('dblclick', sendRequest);
-    document.body.appendChild(timeElem);
+  function createTimeElem(eventHandler, text) {
+    const element = document.createElement('DIV');
+    element.style.display = 'inline-block';
+    element.style.color = '#DC7391';
+    element.style.backgroundColor = '#B4526E';
+    element.style.borderRadius = '3px';
+    element.style.padding = '0px 5px';
+    element.style.cursor = 'pointer';
+    element.classList.add(`time-combined--${text}`);
+    element.title = 'Double click to reload the data';
+    element.addEventListener('dblclick', eventHandler.bind(text, element));
+    return element;
   }
 
-  function showTime(time) {
-    timeoutTimes++;
-    if (timeoutTimes > TIMEOUTLIMIT) return;
-    if (document.body === null) {
-      setTimeout(showTime.bind(null, time), 300);
-      return;
-    }
-    if (timeElem === null) createTimeElem();
-    timeElem.innerHTML = 'Today: ' + Math.floor(time * 100) / 100;
-  }
-
-  function showResponse() {
-    if (this.readyState == 4 && this.status == 200) {
-      var times = [];
-      var combinedTime = 0;
-      try {
-        times = JSON.parse(xhttp.response).timeEntries;
-      } catch (e) {
-        console.log(e);
-        return;
-      }
-      combinedTime = times.reduce((start, obj) => start + obj.hours + (obj.minutes / 60), 0);
-      setTimeout(showTime.bind(null, combinedTime), 300);
-    }
-  }
-
-  xhttp.onreadystatechange = showResponse;
-  sendRequest();
+  todayTimeElement = createTimeElem(getTodaysTime, 'Today');
+  container.style.top = '80px';
+  container.style.left = '50%';
+  container.style.zIndex = 1000;
+  container.style.position = 'absolute';
+  container.classList.add('time-container');
+  container.style.transform = 'translateX(-50%)';
+  container.appendChild(todayTimeElement);
+  document.body.appendChild(container);
+  getTodaysTime('Today', todayTimeElement);
 }());
