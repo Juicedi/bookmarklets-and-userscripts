c19b897f99e2a0d4abab41566d27d639596919d4
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2019-09-20 22:55:52 +0300

    updated development environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'airbnb-base',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    no-param-reassing: 0,
-  },
+  "extends": "airbnb-base",
+  "globals": {
+    "window": "readonly",
+    "XMLHttpRequest": "readonly",
+    "document": "readonly"
+  }
 };
